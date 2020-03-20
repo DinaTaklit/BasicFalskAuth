212f957f8c821a1ca53b658224cf0b1c853af5ad
DinaTaklit <ez_taklit@esi.dz>
2020-03-20 12:52:45 +0100

    feat: Implement AuthError exception class

--- app.py
+++ app.py
@@ -10,3 +10,9 @@ app = Flask(__name__)
 AUTH0_DOMAIN = @TODO_REPLACE_WITH_YOUR_DOMAIN
 ALGORITHMS = ['RS256']
 API_AUDIENCE = @TODO_REPLACE_WITH_YOUR_API_AUDIENCE
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
